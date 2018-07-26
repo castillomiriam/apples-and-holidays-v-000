3b5393c604bfbc862040ad73331b726156aed547
Miriam Castillo <castillo.m.mc@gmail.com>
2018-07-26 05:39:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.map do |season, holiday|
   holiday.map do |holiday, supply_array|
     if supply_array.include?("BBQ")
-      holiday
+      holiday.flatten.compact
 end
 end
 end
